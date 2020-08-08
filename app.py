54e12f098e9b0257017a7422d2d33d1b180a2529
Giannis Tolios <derevirn@gmail.com>
2020-08-09 01:51:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import datetime
 from country_mappings import COUNTRY_MAPPINGS
 from credentials import get_token
 from entsoe import EntsoePandasClient
@@ -13,12 +14,23 @@ country = st.selectbox(
     "Select a country code",
     list(COUNTRY_MAPPINGS.keys()))
 
+date_start = st.date_input(
+    "Enter Start Date",
+    datetime.date(2020,8,1))
+
+date_end = st.date_input(
+    "Enter End Date",
+    datetime.date(2020,8,2))
+
 country_code = COUNTRY_MAPPINGS[country]
 
 client = EntsoePandasClient(api_key=get_token())
-start = pd.Timestamp('20200807', tz=TIMEZONE_MAPPINGS[country_code])
-end = pd.Timestamp('20200808', tz=TIMEZONE_MAPPINGS[country_code])
+start = pd.Timestamp(date_start, tz=TIMEZONE_MAPPINGS[country_code])
+end = pd.Timestamp(date_end, tz=TIMEZONE_MAPPINGS[country_code])
 df = client.query_generation(country_code, start=start,end=end, psr_type=None)
+df = df.resample('H').mean()
 cols = ['Solar', 'Wind Onshore']
 
-st.line_chart(df)
+st.area_chart(df)
+
+st.line_chart(df[cols])
