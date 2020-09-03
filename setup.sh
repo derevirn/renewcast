6345d46f6712899448e3e64c0d6c60c830d79535
Giannis Tolios <derevirn@gmail.com>
2020-09-04 01:06:39 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 mkdir -p ~/.streamlit/
 
-echo “\
+echo "\
 [general]\n\
-email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.toml
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
 
-echo “\
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
