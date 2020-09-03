2f1b41ca25a5cda4226ffa5358639b6f3e7f72bc
Giannis Tolios <derevirn@gmail.com>
2020-09-04 00:51:33 +0300

    Update entsoe_client.py

--- entsoe_client.py
+++ entsoe_client.py
@@ -1,12 +1,15 @@
+import os
 import pandas as pd
 import streamlit as st
-from credentials import get_token
+from dotenv import load_dotenv
 from entsoe import EntsoePandasClient
 from entsoe.mappings import TIMEZONE_MAPPINGS
 
 @st.cache
 def get_energy_data(country_code):
-    client = EntsoePandasClient(api_key=get_token())
+    load_dotenv()
+    token = os.environ['TOKEN']
+    client = EntsoePandasClient(api_key=token)
     end = pd.Timestamp.now(tz=TIMEZONE_MAPPINGS[country_code])
     start = end - pd.DateOffset(months=1)
     df = client.query_generation(country_code, start=start,end=end, psr_type=None)
