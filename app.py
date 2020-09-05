b5f8f67ba2584ff5e4c7cd1c1342633c1d9af64e
Giannis Tolios <derevirn@gmail.com>
2020-09-06 02:07:39 +0300

    Updated app.py and forecast.py

--- app.py
+++ app.py
@@ -1,35 +1,46 @@
 import streamlit as st
 from country_mappings import COUNTRY_MAPPINGS
 from entsoe_client import get_energy_data
-from forecast import generate_forecast
+from forecast import generate_forecast, calculate_smape
 
 st.title("Forecasting Renewable Energy Production in EU")
+st.markdown("Select a country to view a chart of total renewable energy production,\
+        as well as production forecasts for each type. You can also select the regression algorithm, \
+        the forecast horizon, and the window length.")
 
-country = st.sidebar.selectbox("Select a Country",
-    list(COUNTRY_MAPPINGS.keys()))
+country = st.sidebar.selectbox(label = "Select a Country", index = 9,
+   options = list(COUNTRY_MAPPINGS.keys()))
 
 regressor = st.sidebar.selectbox("Select a Regression Algorithm",
              ['Linear Regression', 'K-Nearest Neighbors', 'Random Forest',
-              'Gradient Boosting', 'Support Vector Machines' ])                    
+              'Gradient Boosting', 'Support Vector Machines', 'Extra Trees' ])                    
 
-st.header(country)
+st.subheader('Total Renewable Energy Production in ' + country + ' (MW)') 
 
 forecast_horizon = st.sidebar.slider(label='Forecast Horizon (hours)',
                              min_value = 12, max_value = 168,
                              value = 48)
 
 window_length = st.sidebar.slider(label = 'Window Length', min_value = 1,
-                          value = 5)
+                          value = 10)
 
 
 country_code = COUNTRY_MAPPINGS[country]
 df = get_energy_data(country_code)
 
-st.area_chart(df[['Solar', 'Wind Onshore']])
+cols_renewable = [ 'Wind Onshore', 'Wind Offshore', 'Solar', 'Biomass', 'Geothermal',
+                   'Hydro Pumped Storage', 'Hydro Water Reservoir' ] 
 
+df = df[df.columns & cols_renewable]
 
-df_wind = generate_forecast(df[['Wind Onshore']], regressor, forecast_horizon, window_length)
-df_solar = generate_forecast(df[['Solar']], regressor, forecast_horizon, window_length)
+st.area_chart(df, use_container_width = False, width = 800)
 
-st.line_chart(df_wind)
-st.line_chart(df_solar)
+for item in df.columns:
+    
+    smape = calculate_smape(df[[item]], regressor, forecast_horizon, window_length)
+    st.subheader(item + ' Energy Production Forecast in ' + country + ' (MW)')
+    
+    df_forecast = generate_forecast(df[[item]], regressor, forecast_horizon, window_length)
+    st.line_chart(df_forecast, use_container_width = False, width = 800)
+    
+    st.text('SMAPE: %.2f' % smape)
--- forecast.py
+++ forecast.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
 from sktime.forecasting.compose import ReducedRegressionForecaster
-from sktime.utils.plotting.forecasting import plot_ys
+from sktime.forecasting.model_selection import temporal_train_test_split
+from sktime.performance_metrics.forecasting import smape_loss
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
 from sklearn.svm import LinearSVR
 
 
@@ -15,14 +16,16 @@ def select_regressor(selection):
     'Random Forest': RandomForestRegressor(),
     'Gradient Boosting': GradientBoostingRegressor(),
     'Support Vector Machines': LinearSVR(),
+    'Extra Trees': ExtraTreesRegressor(),
     
      }
     return regressors[selection]
 
 
 
-def generate_forecast(df_, regressor, forecast_horizon = 24, window_length = 5):
+def generate_forecast(df_, regressor, forecast_horizon, window_length):
     df = df_.copy()
+    df.fillna(method = 'ffill', inplace = True)
     y_train = df.iloc[:,0].reset_index(drop=True)
     fh = np.arange(forecast_horizon) + 1
     regressor = select_regressor(regressor)
@@ -40,3 +43,18 @@ def generate_forecast(df_, regressor, forecast_horizon = 24, window_length = 5):
     df = df.append(df_pred, ignore_index=True)
     df.set_index(date_index, inplace=True)
     return df
+
+def calculate_smape(df_, regressor, forecast_horizon, window_length):
+    df = df_.copy()
+    df.fillna(method = 'ffill', inplace = True)
+    y = df.iloc[:,0].reset_index(drop=True)
+    y_train, y_test = temporal_train_test_split(y, test_size = forecast_horizon)
+    fh = np.arange(y_test.shape[0]) + 1
+    regressor = select_regressor(regressor)
+    forecaster = ReducedRegressionForecaster(regressor=regressor, window_length=window_length,
+                                             strategy='recursive')
+    forecaster.fit(y_train, fh=fh)
+    y_pred = forecaster.predict(fh)
+    
+    return smape_loss(y_pred, y_test)
+                                     
